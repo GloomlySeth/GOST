4449fd2df8590584fd3ac77d0d523ae95a342c77
dmowski <dmowski@yandex.ru>
2016-06-08 17:05:58 +0300

    Перенос верстки на скелет шаблона

--- sites/all/themes/gost/page.tpl.php
+++ sites/all/themes/gost/page.tpl.php
@@ -1,4 +1,108 @@
 <?php
-// $Id: node.tpl.php,v 1.24 2010/12/01 00:18:15 webchick Exp $
 ?>
-PAGE.TPL.PHPPPPPPPPP
+<?php print render($page['header']); ?>
+
+  <div class="header">
+      <div class = "row topMenu">
+
+            <div class="col-xs-2 someMargin1" style="margin: 15px 0px 0px;">
+                <!-- Logo (Image) -->
+                <div id="u28" class="ax_image" data-label="Logo">
+                    <img id="u28_img" class="img " src="/<?=$directory;?>/images/main_page/logo_u34.png"/>
+                </div>
+
+                <!-- Logo title (Shape) -->
+                <div id="u30" class="ax_paragraph" data-label="Logo title">
+                    <div id="u31" class="text">
+                        <p>
+                            <span>РЕДАКТОР ДОКУМЕНТОВ</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Unnamed (Menu) -->
+            <div id="u8" class="ax_menu col-xs-8">
+
+                <!-- Unnamed (Table) -->
+                <div id="u9" class="ax_table">
+                    <!-- Unnamed (Menu Item) -->
+                    <!-- Unnamed (Menu Item) -->
+                    <!-- Unnamed (Menu Item) -->
+                </div>
+            </div>
+
+            <!-- Unnamed (Menu) -->
+            <div id="u16" class="ax_menu col-xs-2">
+                <!-- Unnamed (Table) -->
+                <div id="u17" class="ax_table">
+                    <!-- Unnamed (Menu Item) -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="midCont">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="menuClassParent">
+            <div class="col-md-3 menuClassOne sidebarUsr" >
+              <div>
+                <!-- Меню, которое слева -->
+              </div>
+            </div>
+
+            <div class="col-md-9">
+                <?php print render($page['content']); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="footer">
+      <div class="row rowEdit" >
+            <!-- Unnamed (Shape) -->
+            <div id="u32" class="ax_shape">
+
+
+            <div class="col-xs-2"></div>
+
+            <!-- Unnamed (Menu) -->
+            <div id="u36" class="ax_menu col-xs-8">
+                <!-- Unnamed (Table) -->
+                <div id="u37" class="ax_table">
+
+                    <!-- Unnamed (Menu Item) -->
+                    <div id="u38" class="ax_table_cell">
+                        <div id="u39" class="text">
+                            <p>
+                                <span style="text-decoration:underline;">FAQ</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Unnamed (Menu Item) -->
+                    <div id="u40" class="ax_table_cell">
+                        <div id="u41" class="text">
+                            <p>
+                                <span style="text-decoration:underline;">ПОДДЕРЖКА</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Unnamed (Menu Item) -->
+                    <div id="u42" class="ax_table_cell">
+                        <div id="u43" class="text">
+                            <p>
+                                <span style="text-decoration:underline;">О НАС</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
