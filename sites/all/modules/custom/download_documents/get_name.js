7d42145c415bb2fcabd3348987e1eab964d48a55
Rynkevich17 <rynkevich17@tut.by>
2016-12-27 19:57:50 +0300

    Download documents

---
+++ sites/all/modules/custom/download_documents/get_name.js
@@ -0,0 +1,11 @@
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }						
+    var filename = str.slice(i);			
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
