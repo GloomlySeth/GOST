18a0b37e3991a9143a0f141a5d75974e6118d505
Rynkevich17 <rynkevich17@tut.by>
2017-08-17 20:55:06 +0300

    Funktion t

--- sites/all/themes/gost/template.php
+++ sites/all/themes/gost/template.php
@@ -2,7 +2,7 @@
 
 function gost_preprocess_views_view(&$vars){
   if($vars['view']->name === 'requirements_list'){
-    $vars['rows']['select']['action::views_bulk_operations_delete_item']['#value'] = 'Удалить...';
+    $vars['rows']['select']['action::views_bulk_operations_delete_item']['#value'] = t('Удалить...');
 	$vars['rows']['select']['action::views_bulk_operations_delete_item']['#attributes']['class'][] = 'demand_del';
 	
   }
@@ -12,7 +12,7 @@ function gost_preprocess_views_view_table(&$vars) {
   if($vars['view']->name === 'requirements_list'){
     global $user;
     foreach($vars['rows'] as $key => $value){
-      $value['nothing'] == $user->uid ? $vars['rows'][$key]['nothing'] = 'Мое' : $vars['rows'][$key]['nothing'] = 'Стандартное';
+      $value['nothing'] == $user->uid ? $vars['rows'][$key]['nothing'] = t('Мое') : $vars['rows'][$key]['nothing'] = t('Стандартное');
     }
   }
 }
