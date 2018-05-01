9ce5786ffadf8d46a34ec5bd342d3eb72772c5c4
Rynkevich17 <rynkevich17@tut.by>
2018-05-01 19:54:16 +0300

    ISSUE-22 | Added blocks fonts in demand create

--- sites/all/themes/gost/js/demandSelect.js
+++ sites/all/themes/gost/js/demandSelect.js
@@ -3,7 +3,7 @@
     $('select.demandSelect').css("background-color", "black");
 	$(document).ready(function(){
 		$('select.demandSelect').change(function (){
-		var idSelect = '#' + $(this).attr("id") + ' option:selected';//alert(idSelect);
+		var idSelect = '#' + $(this).attr("id") + ' option:selected';
 		$(this).css("background-color", $(idSelect).css("background-color"));
 		});
 	});
