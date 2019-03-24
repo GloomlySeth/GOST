53a9fb3f64965a955de0d33def8ae9a70ed77dc4
TemkaHD <TemkaHD@gmail.com>
2019-03-24 15:40:50 +0300

    rafactor temlate

--- sites/all/themes/gost/templates/page--front.tpl.php
+++ sites/all/themes/gost/templates/page--front.tpl.php
@@ -11,10 +11,7 @@
 <div class="header">
   <div class="grid__center">
     <div class="header__logo">    
-<!--      <a href="--><?php //print $front_page; ?><!--" title="--><?php //print t('Home'); ?><!--" rel="home" >-->
-<!--        <img src="/--><?php //print $directory; ?><!--/images/logo.png" alt="--><?php //print t('Home'); ?><!--" />-->
-<!--        <span class="header__site-name">--><?php //print $site_name; ?><!--</span>-->
-<!--      </a>-->
+
     </div>
     <div class="header__menu">
       <?php print render($page['header']); ?>
